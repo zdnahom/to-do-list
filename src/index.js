5bb2d078b3d8fca4585da041b245ae78d52894d2
Nahom <zerihunnahom@gmail.com>
2023-02-21 17:00:34 +0300

    Add populateTodos functionality

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const todoLists=[
+    {description:"Take a nap",completed:false,index:0},
+    {description:"Workout",completed:false,index:1},
+    {description:"Finish today's project",completed:false,index:2},
+]
+function populateTodos(data){
+    data.forEach(element => {
+        const li = document.createElement('li')
+        li.innerHTML=`${element.description}`
+        document.querySelector('.tasks').appendChild(li)
+    });
+}
+populateTodos(todoLists)

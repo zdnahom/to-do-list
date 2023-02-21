fded34e49a7e049f80665ac0fa9bba2e2e7da86a
Nahom <zerihunnahom@gmail.com>
2023-02-21 16:45:20 +0300

    Add html-webpack-plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path=require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports={
-    
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:path.resolve(__dirname,'src','index.html')
+        })
+    ]
 }

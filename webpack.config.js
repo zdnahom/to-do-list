934ed419907de9651bf1d889e9ae20596dab7208
Nahom <zerihunnahom@gmail.com>
2023-02-21 22:04:33 +0300

    Fix linter error

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path=require('path')
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports={
-    module:{
-        rules:[
-            {
-                test:/\.css$/,
-                use:["style-loader","css-loader"]
-            }
-        ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template:path.resolve(__dirname,'src','index.html')
-        })
-    ]
-}
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+};

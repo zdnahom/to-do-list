0e8f61e69395478f05894a06a4722293c70d77b2
Nahom <zerihunnahom@gmail.com>
2023-02-21 17:59:16 +0300

    Setup the loaders for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ const path=require('path')
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports={
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:["style-loader","css-loader"]
+            }
+        ]
+    },
     plugins:[
         new HtmlWebpackPlugin({
             template:path.resolve(__dirname,'src','index.html')

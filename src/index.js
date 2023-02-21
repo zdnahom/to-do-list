70a5b71e8d6dfd59536be5e600826631679158de
Nahom <zerihunnahom@gmail.com>
2023-02-21 21:52:26 +0300

    Refactor to do list array and li element

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import "./style.css"
 
 const todoLists=[
-    {description:"Take a nap",completed:false,index:0},
-    {description:"Workout",completed:false,index:1},
-    {description:"Finish today's project",completed:false,index:2},
+    {description:"wash the dishes",completed:false,index:0},
+    {description:"complete To Do list project",completed:false,index:1},
 ]
 function populateTodos(data){
     data.forEach(element => {
         const li = document.createElement('li')
         li.innerHTML=`
         <div>
-        <input type="checkbox" name="" id="">
+        <i class="fa-regular fa-square"></i>
         <span>${element.description}</span>
         </div>
         <i class="fa-solid fa-ellipsis-vertical"></i>

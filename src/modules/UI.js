f53bc7413c3a8205c4608fe8f785cb37118343c0
Nahom <zerihunnahom@gmail.com>
2023-02-22 22:01:30 +0300

    Add add todo functionality

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,5 +2,17 @@ export class UI{
     static remove=(element)=>{
         element.parentNode.parentNode.remove()
     }
-
+    static add = (id,description) => {
+        const li = document.createElement("li");
+        li.innerHTML =`
+            <div>
+            <input type="checkbox" name="checkbox">
+            <span>${description}</span>
+            </div>
+            <button type="button">
+            <i class="fa-solid fa-trash" id=${id}></i>
+            </button>
+            `;
+        document.querySelector(".tasks").appendChild(li);
+      };
 }

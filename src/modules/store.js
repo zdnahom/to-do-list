5a19b9496bcf4bfa5b756fe34bbca74c731c4bf6
Nahom <zerihunnahom@gmail.com>
2023-02-22 16:04:29 +0300

    Add a function to set todos on localstorage

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,3 +1,6 @@
-export default getTodos=()=>{
+export const getTodos=()=>{
     return JSON.parse(localStorage.getItem("todos"))
+}
+export const setTodos=(obj)=>{
+   localStorage.setItem("todos",JSON.stringify(obj))
 }

653eda27bfc36b9c9d1f683020460e6c2f0a1f4e
Nahom <zerihunnahom@gmail.com>
2023-02-22 16:44:43 +0300

    Add event handler to the form

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ const todoLists = [
   { description: 'wash the dishes', completed: false, index: 0 },
   { description: 'complete To Do list project', completed: false, index: 1 },
 ];
+
 const container=new TodosContainer(); 
 
 const form =document.querySelector('.task-adder form')
+const {task}=form.elements
+
 function populateTodos(data) {
   data.forEach((element) => {
     const li = document.createElement('li');
@@ -21,6 +24,9 @@ function populateTodos(data) {
     document.querySelector('.tasks').appendChild(li);
   });
 }
-
+form.addEventListener("submit",(event)=>{
+  event.preventDefault()
+  container.addTodo(task.value)
+})
 
 populateTodos(todoLists);

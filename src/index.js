df59c4eec0cbce40aa7c9cc2119a05ac19f33fb3
Nahom <zerihunnahom@gmail.com>
2023-02-22 16:24:52 +0300

    Import todos container class and create an instance

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './style.css';
+import TodosContainer from './modules/TodosContainer';
 
 const todoLists = [
   { description: 'wash the dishes', completed: false, index: 0 },
   { description: 'complete To Do list project', completed: false, index: 1 },
 ];
+const container=new TodosContainer(); 
+
 function populateTodos(data) {
   data.forEach((element) => {
     const li = document.createElement('li');
@@ -17,4 +20,6 @@ function populateTodos(data) {
     document.querySelector('.tasks').appendChild(li);
   });
 }
+
+
 populateTodos(todoLists);

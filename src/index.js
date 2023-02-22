64a09b0fee14b135ae6c27f7b0953e40bdb08517
Nahom <zerihunnahom@gmail.com>
2023-02-22 22:12:29 +0300

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,38 +1,28 @@
-import './style.css';
-import { getTodos } from './modules/store';
-import TodosContainer from './modules/TodosContainer';
+import "./style.css";
+import { getTodos } from "./modules/store";
+import TodosContainer from "./modules/TodosContainer";
+import { UI } from "./modules/UI";
 
-const todoLists = getTodos()|| [];
-const container=new TodosContainer(); 
+const todoLists = getTodos() || [];
+const container = new TodosContainer();
 
-const form =document.querySelector('.task-adder form')
-const todoElements=document.querySelector('.tasks')
-console.log(todoElements)
-const {task}=form.elements
+const form = document.querySelector(".task-adder form");
+const todoElements = document.querySelector(".tasks");
+const { task } = form.elements;
 
 function populateTodos(data) {
   data.forEach((element) => {
-    const li = document.createElement('li');
-    li.innerHTML = `
-        <div>
-        <input type="checkbox" name="checkbox">
-        <span>${element.description}</span>
-        </div>
-        <button type="button">
-        <i class="fa-solid fa-trash" id=${element.index}></i>
-        </button>
-        `;
-    document.querySelector('.tasks').appendChild(li);
+    UI.add(element.id, element.description);
   });
 }
-form.addEventListener("submit",(event)=>{
-  event.preventDefault()
-  container.addTodo(task.value)
-})
-todoElements.addEventListener("click",(event)=>{
-  if(event.target.id){
-    container.removeTodo(event.target.id)
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  container.addTodo(task.value);
+});
+todoElements.addEventListener("click", (event) => {
+  if (event.target.id) {
+    container.removeTodo(event.target.id);
   }
-})
-// localStorage.clear()
+});
+
 populateTodos(todoLists);

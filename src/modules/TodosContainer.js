d1d537a707c00a30a6d0a8c3ddbf42a4cfa118ef
Nahom <zerihunnahom@gmail.com>
2023-02-22 18:16:31 +0300

    Implement removeTodo functionality

--- src/modules/TodosContainer.js
+++ src/modules/TodosContainer.js
@@ -1,11 +1,19 @@
-import Todo from "./Todo.js"
-import {getTodos,setTodos} from "./store.js"
-export default class TodosContainer{
-    todos= getTodos()|| [];
-    addTodo=(description)=>{
-        const index=this.todos.length + 1
-        const todo=new Todo(description,false,index);
-        this.todos.push(todo)
-        setTodos(this.todos)
-    }
+import Todo from "./Todo.js";
+import { getTodos, setTodos } from "./store.js";
+export default class TodosContainer {
+  todos = getTodos() || [];
+  addTodo = (description) => {
+    const index = this.todos.length + 1;
+    const todo = new Todo(description, false, index);
+    this.todos.push(todo);
+    setTodos(this.todos);
+  };
+  removeTodo = (index) => {
+    this.todos = this.todos
+      .filter((todo) => todo.index !== parseInt(index))
+      .map((todo, index) => {
+        return { ...todo, index: index + 1 };
+      });
+      setTodos(this.todos)
+  };
 }

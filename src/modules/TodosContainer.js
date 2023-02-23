b0b80fc432bcdac9b93fe8d8441b6e472e39e22f
Nahom <zerihunnahom@gmail.com>
2023-02-24 00:29:40 +0300

    Refactor add , remove,update,methods

--- src/modules/TodosContainer.js
+++ src/modules/TodosContainer.js
@@ -3,20 +3,20 @@ import { getTodos, setTodos } from './store.js';
 import UI from './UI.js';
 
 export default class TodosContainer {
-  todos = getTodos() || [];
-
   addTodo = (description) => {
+    this.todos = getTodos() || [];
     if (description.trim().length !== 0) {
       const id = (Math.floor(Math.random() * 100000)).toString();
       const index = this.todos.length + 1;
       const todo = new Todo(id, description, false, index);
       this.todos.push(todo);
       setTodos(this.todos);
-      UI.add(id, description);
+      UI.add(id, description,false);
     }
   };
 
   removeTodo = (id) => {
+    this.todos = getTodos() || [];
     this.todos = this.todos
       .filter((todo) => todo.id !== id)
       .map((todo, index) => ({ ...todo, index: index + 1 }));
@@ -25,6 +25,7 @@ export default class TodosContainer {
   };
 
   updateDescription=(id, description) => {
+    this.todos = getTodos() || [];
     const [updateId] = id.match(/\d+/);
     const updateDes = description.match(/\w+/g) ? description.match(/\w+/g).join(' ') : '';
     if (updateDes.length > 0) {

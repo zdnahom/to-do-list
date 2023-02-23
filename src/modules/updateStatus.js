a8ce109c2f743468e25eb8623fa2d5c5741d15e5
Nahom <zerihunnahom@gmail.com>
2023-02-23 17:10:23 +0300

    Add update functionality

--- src/modules/updateStatus.js
+++ src/modules/updateStatus.js
@@ -1,4 +1,12 @@
 import { getTodos ,setTodos} from "./store.js"
 export const updateStatus=(id)=>{
-    console.log(id)
+    const [statusId] = id.match(/\d+/);
+    const todos=getTodos().map(todo=>{
+        if(todo.id===statusId){
+            return {...todo,completed:!todo.completed}
+        }else{
+            return todo
+        }
+    })
+    setTodos(todos)
 }

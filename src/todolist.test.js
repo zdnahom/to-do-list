f50f554584212c1db7c7f758988a82a77a687491
Nahom <zerihunnahom@gmail.com>
2023-03-02 21:19:47 +0300

    Refactor code to add new testcases

--- src/todolist.test.js
+++ src/todolist.test.js
@@ -1,76 +1,124 @@
-import TodosContainer from './modules/TodosContainer.js';
+import TodosContainer from "./modules/TodosContainer.js";
+import { getTodos } from "./modules/store";
+import { updateStatus, clearTodoList } from "./modules/updateStatus.js";
 
 // Set up our document body
 document.body.innerHTML = `
 <ul class="tasks">
-<li>
-<div>
-<input type="checkbox" name="checkbox" id="checkbox-1">
-<input type="text" name="task_description" id="description-1" value="Swim"/>
-</div>
-<button type="button">
-<i class="fa-solid fa-trash" id="1"></i>
-</button>
-</li>
 </ul>
 `;
 
-describe('Test Add todo', () => {
-  it('Add a valid todo', () => {
+describe("Test Add todo", () => {
+  it("Add a valid todo", () => {
     // arrange
     const container = new TodosContainer();
 
     // act
-    container.addTodo('read books');
-    container.addTodo('dance');
-    container.addTodo('code for 1 hour');
-    const tasks = document.querySelectorAll('.tasks li');
+    container.addTodo("read books");
+    container.addTodo("dance");
+    container.addTodo("code for 1 hour");
+    container.addTodo("Eat salad");
+    const tasks = document.querySelectorAll(".tasks li");
 
     // assert
     expect(tasks).toHaveLength(4);
   });
 
-  it('Add empty todo', () => {
+  it("Add empty todo", () => {
     // arrange
     const container = new TodosContainer();
 
     // act
-    container.addTodo('  ');
-    const tasks = document.querySelectorAll('.tasks li');
+    container.addTodo("  ");
+    const tasks = document.querySelectorAll(".tasks li");
 
     // assert
     expect(tasks).toHaveLength(4);
   });
 });
 
-describe('Test Remove todo', () => {
-  it('Remove a todo with id=1', () => {
+describe("Test Remove todo", () => {
+  it("Remove a todo with id=1", () => {
     // arrange
     const container = new TodosContainer();
+    let tasks = document.querySelectorAll(".tasks li");
+    const task = tasks[0].querySelector('i');
+
     // act
-    container.removeTodo('1');
-    const tasks = document.querySelectorAll('.tasks li');
+    container.removeTodo(task.id);
+    tasks = document.querySelectorAll(".tasks li");
+
     // assert
     expect(tasks).toHaveLength(3);
   });
 });
 
-describe('Test Update todo', () => {
-  it('Update todo item description', () => {
+describe("Test Update todo", () => {
+  it("Update todo item description", () => {
     // arrange
     const container = new TodosContainer();
-    container.addTodo('read books');
-    container.addTodo('dance');
-    container.addTodo('code for 1 hour');
+    const tasks = document.querySelectorAll(".tasks li");
+    const task = tasks[0].querySelector('input[type="text"]');
 
     // act
-    container.updateDescription('1', 'Read more books');
+    container.updateDescription(task.id, "Read more books");
 
     // assert
-    const updatedTasks = document.querySelectorAll('.tasks li');
-    const updatedFirstInput = updatedTasks[0].querySelector('input[type="text"]');
-    expect(updatedFirstInput.value).toEqual('Read more books');
+    expect(task.value).toEqual("Read more books");
+  });
+
+  it("Update todo item description with empty string", () => {
+    // arrange
+    const container = new TodosContainer();
+    let tasks = document.querySelectorAll(".tasks li");
+    const task = tasks[0].querySelector('input[type="text"]');
+
+    // act
+    container.updateDescription(task.id, "");
+    tasks = document.querySelectorAll(".tasks li");
+
+    // assert
+    expect(tasks).toHaveLength(2);
   });
 });
 
+describe("Test for completed status", () => {
+  it("change todo list status to true", () => {
+    //arrange
+    const tasks = document.querySelectorAll(".tasks li");
+    const task1 = tasks[0].querySelector('input[type="checkbox"]');
+    const task2 = tasks[1].querySelector('input[type="checkbox"]');
+    
+    //act
+    updateStatus(task1.id);
+    updateStatus(task2.id);
+    
+    //assert
+    expect(getTodos()[0].completed).toBe(true);
+  });
+  it("change todo list status to false", () => {
+    //arrange
+    const tasks = document.querySelectorAll(".tasks li");
+    const task = tasks[0].querySelector('input[type="checkbox"]');
 
+    //act
+    updateStatus(task.id);
+
+    //assert
+    expect(getTodos()[0].completed).toBe(false);
+  });
+});
+
+describe("Test for Clear completed", () => {
+  it("clear completed tasks",()=>{
+    //arrange
+    let tasks = document.querySelectorAll(".tasks li");
+    
+    //act
+    clearTodoList()
+    tasks = document.querySelectorAll(".tasks li");
+
+    //assert
+    expect(tasks).toHaveLength(1)
+  })
+});
